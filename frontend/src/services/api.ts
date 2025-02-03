63925ae1cd8d9229de510764a8577ef9bc5f277c
Filipe Garcia <filipe.garcia1997@gmail.com>
2025-02-02 21:07:27 -0300

    feat: update with temporary api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://guess-football-team-backend.vercel.app';
+const API_URL = 'https://guess-football-team.onrender.com/api';
 
 export const getSquads = async () => {
     const response = await axios.get(`${API_URL}/squads`);
